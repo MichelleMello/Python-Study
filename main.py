9ed6477ce01974d72ce31c36fd9bc8a0b1bedc3c
Michelle Mello <mello.de.michelle@gmail.com>
2021-10-12 19:49:26 -0300

    Strings study

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 character_name = "John"
 character_age = "35"
+isMale = True
 
 print("There was a man named " + character_name + ", ")
 print("he was " + character_age + " years old.")
+
+character_name = "Mike"
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age)
+
+print("Giraffe\nAcademy")
